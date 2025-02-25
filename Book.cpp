29d5458c20dfcabb2c549a7de6e2a8cfc5d32c5a
Khanh <ngokhanh809@gmail.com>
2025-02-25 10:54:24 -0800

    added getters and setters

--- Book.cpp
+++ Book.cpp
@@ -8,3 +8,35 @@ Book::Book(string t, string a, bool b){
     author = a;
     isBorrowed = b;
 }
+
+
+
+string Book::getTitle() 
+{
+    return title;
+}
+
+string Book::getAuthor()
+{
+    return author;
+}
+
+bool Book::getBorrowedStatus()
+{
+    return isBorrowed;
+}
+
+void Book::setTitle(string t)
+{
+    title = t;
+}
+
+void Book::setAuthor(string a)
+{
+    author = a;
+}
+
+void Book::setBorrowedStatus(bool b)
+{
+    isBorrowed = b;
+}
