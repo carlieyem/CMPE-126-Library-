10f8e283b4c444ef4289efc63d3bb41a970b29cc
Khanh <ngokhanh809@gmail.com>
2025-02-25 11:02:38 -0800

    added >, <, == operators

--- Book.cpp
+++ Book.cpp
@@ -45,3 +45,18 @@ void Book::setBorrowedStatus(bool b)
     isBorrowed = b;
 }
 
+bool operator==(Book b1, Book b2)
+{
+    return b1.title == b2.title && b1.author == b2.author;
+}
+
+bool operator<(Book b1, Book b2)
+{
+    return b1.title[0] < b2.title[0];
+}
+
+bool operator>(Book b1, Book b2)
+{
+    return b1.title[0] > b2.title[0];
+}
+
