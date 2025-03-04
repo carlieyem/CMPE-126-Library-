872858266547b64393d2655ba8da57e1a5c97ab2
DavidJohnsonVTA <djwing88@gmail.com>
2025-03-03 22:13:37 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,48 +1,31 @@
 #include <iostream>
-#include "Book.h"
+#include "Library.h"
 
 using namespace std;
 
 int main() {
-    
-    Book book1("War and Peace", "Leo Tolstoy");
-    Book book2("Heir to the Empire", "Timothy Zahn");
-    Book book3("The Great Gatsby", "F. Scott Fitzgerald", true);
 
-    cout << "Book 1 Title: " << book1.getTitle() << ", Author: " << book1.getAuthor()
-    <<", Borrowed: " << (book1.getBorrowedStatus() ? "Yes" : "No") << endl;
+    Library myLib;
 
-    cout << "Book 2 Title: " << book2.getTitle() << ", Author: " << book2.getAuthor()
-    <<", Borrowed: " << (book2.getBorrowedStatus() ? "Yes" : "No") << endl;
+    myLib.addBook(Book("War and Peace", "Leo Tolstoy"));
+    myLib.addBook(Book("Heir to the Empire", "Timothy Zahn"));
+    myLib.addBook(Book("The Great Gatsby", "F. Scott Fitzgerald"));
 
-    cout << "Book 3 Title: " << book3.getTitle() << ", Author: " << book3.getAuthor()
-    <<", Borrowed: " << (book3.getBorrowedStatus() ? "Yes" : "No") << endl;
+    cout << "Library Collection: \n";
+    myLib.displayBooks();
 
-    cout << "Comparing books:" << endl;
-    
-    if (book1 == book3) {
-        cout << "Book 1 and Book 3 are the same." << endl;
-    } else {
-        cout << "Book 1 and Book 3 are different." << endl;
-    }
+    Book newBook("Rangers Apprentice", "John Flanagan");
+    myLib.addBook(newBook);
 
-    if (book1 < book2) {
-        cout << "Book 1 comes before Book 2 alphabetically." << endl;
-    } else {
-        cout << "Book 2 comes before Book 1 alphabetically." << endl;
-    }
+    cout << "Updated Library Collection: \n";
+    myLib.displayBooks();
 
-    if (book1 > book2) {
-        cout << "Book 1 comes after Book 2 alphabetically." << endl;
-    } else {
-        cout << "Book 2 comes after Book 1 alphabetically." << endl;
-    }
-    cout << "Updating Book 2's status..." << endl;
-    book2.setBorrowedStatus(true);
-    cout << "Book 2 Borrowed Status: " << (book2.getBorrowedStatus() ? "Yes" : "No") << endl;
+    cout << "Removing 'War and Peace'\n";
+    myLib.removeBook("War and Peace");
 
-    const char *BookTypeName[] = { "none", "Ebook", "printed book"};
-    cout << "Book 1's type: " << BookTypeName[book1.getType()] << '\n';
+    cout << "Library Collection: \n";
+    myLib.displayBooks();
 
     return 0;
+
 }
